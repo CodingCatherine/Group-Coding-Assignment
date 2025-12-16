5c9def1fa7673c40b2b6cb11b07f840605e572aa
Catherine <>
2025-12-15 23:44:37 -0500

    Adding some required things for the rubric

--- src/tips.java
+++ src/tips.java
@@ -18,89 +18,176 @@ public class tips extends javax.swing.JFrame {
      */
     public tips() {
         initComponents();
+        //Make the continue button invisible until the user enters a tip
         cont.setVisible(false);
         
     }
     
-    public static File file = new File ("tips.txt");
-    public static int numTips;
+    /**
+     *Global Variables
+     */
+    public static File file = new File ("tips.txt"); //Variable to hold the file
+    public static int numTips; //Variable to hold the number of tips in the system
     
+    /**
+     * The stay safe class holds tips that the user enters regarding staying 
+     * safe online
+     * @Author Catherine Lin
+     * @since 2025-12-15
+     */
     public class staySafe {
-        private Description description;
-        private String name;
-        private final String DEFAULTNAME = "Anonymous";
+        //Attributes
+        private Description description; //Description object to hold the description of the tip
+        private String name; //Name to hold the name of the user who has submitted the tip
+        private final String DEFAULTNAME = "Anonymous"; //String to hold the default name if the user wants to stay anonymous
         
+        /**
+         * Constructor for the staySafe class if user wants to stay anonymous
+         * @param description of the tip the user has entered
+         */
         public staySafe (Description description){
-            this.description = description;
-            this.name = DEFAULTNAME;
-            numTips++;
+            this.description = description; //Assign description to it's attribute
+            this.name = DEFAULTNAME; //Assign default name to it's attribute
+            numTips++; //Add a tip to the count
         }
-        
+        /**
+         * Constructor for the staySafe class if the user wants to provide their name
+         * @param description of the tip the user has entered
+         * @param name the user's name
+         */
         public staySafe (Description description, String name){
-            this.description = description;
-            this.name = name;
-            numTips++;
+            this.description = description;//Assign description to it's attribute
+            this.name = name; //Assign the user's name to it's attribute
+            numTips++;//Add a tip to the count
             
         }
         
+        /**
+         * Gets the description of the tip
+         * @return the description of the selected tip
+         */
         public Description getDescription(){
             return description;
         }
         
+        /**
+         * Gets the name of the user that has submitted the tip
+         * @return the name of the user that has submitted the specified tip
+         */
         public String getName(){
             return name;
         }
-        
+
+        /**
+         * To string to return the object in readable language
+         * @return the string representation of the object
+         */
         public String toString (){
             return name + "," + description.toString();
+        }
+        
+        /**
+         * A pretty to string to return the object but with better formatting
+         * @return a string representation of the object but more readable for the user
+         */
+        public String toPrettyString(){
+           return description.toPrettyString() + "<br><br> Submitted by: " + name + "</html>";
         }
             
     }
     
+    /**
+     * Description objects describe the tips that users submit
+     * @Author Catherine Lin
+     * @since 2025-12-15
+     */
     public class Description{
-        private String details;
-        private String category;
+        //Attributes
+        private String details;//Details of what the tip is 
+        private String category;//What is the tip about
         
+        /**
+         * Description constructor (Creates the description object)
+         * @param details holds the details of what the tip is about
+         * @param category holds the general category of the tip
+         */
         public Description(String details, String category){
-            this.details = details;
+            //Assigns the inputted values to their respective attributes
+            this.details = details; 
             this.category = category;
             
         }
         
+        /**
+         * Gets the details of the tip
+         * @return the details of the specified tip
+         */
         public String getDetails(){
             return details;
         }
         
+        /**
+         * Gets the category of the tip
+         * @return the specific category of the specified tip
+         */
         public String getCategory(){
             return category;
         }
         
+        /**
+         * To string to return the object in readable language
+         * @return the string representation of the object
+         */
         public String toString(){
             return category + "," + details;
         }
         
+        /**
+         * A pretty to string to return the object but with better formatting
+         * @return a string representation of the object but more readable for the user
+         */
+        public String toPrettyString(){
+            return "<html> For the Category " + category + "<br><br> A Tip is to " + details;
+        }
+        
     }
     
+    /**
+     * Method that creates description objects
+     * @return 
+     */
     public Description desc(){
+        //recieves input from the combo box and assigns it to a variable
         String cat = (String)box.getSelectedItem();
+        //receives input from the text field and assigns it to a variable
         String det = tf1.getText();
+        //Instantiate the description object
         Description desc = new Description (det, cat); 
+        //Return instantiated object
         return desc;
     }
     
+    /**
+     * Adds the specified tip to a flat file
+     * @param desc requires the user's description of their tip
+     */
     public void addTips(Description desc){
-        staySafe tip;
+        staySafe tip;//Create a staySafe object first)
+        //If the user has left the text field blank call on the constructor without name (they want to be anonymous)
         if (tf2.getText().trim().equals("")){
             tip = new staySafe(desc);
         }else{
+            //If not the user has inputted their name so you have to retrieve that input and use the other constructor
             tip = new staySafe(desc, tf2.getText());
         }
+        //Write the user's tip into the flat file
         try{
-            FileWriter write1 = new FileWriter(file, true);
-            PrintWriter output = new PrintWriter(write1);
-            output.println(tip.toString());
-            output.close();
+            FileWriter write1 = new FileWriter(file, true); //Initialize filewriter
+            PrintWriter output = new PrintWriter(write1); //Initialize printwriter
+            output.println(tip.toString()); //print the object to the file with the formatted toString
+            output.close(); //close printwriter
             
+         //If exception is thrown print out an error code
         }catch(IOException e){
             System.out.println("IO Error");
         }
@@ -236,13 +323,16 @@ public class tips extends javax.swing.JFrame {
     }//GEN-LAST:event_tf1ActionPerformed
 
     private void finActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_finActionPerformed
+        //When button is pressed save the user's tip
         addTips(desc());
+        //Display the continue button and hide the finished button so the user can only submit one tip
         cont.setVisible(true);
         fin.setVisible(false);
         
     }//GEN-LAST:event_finActionPerformed
 
     private void contActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_contActionPerformed
+        //Close the jframe and make the next one visible
         new End().setVisible(true);
         this.setVisible(false);
         
